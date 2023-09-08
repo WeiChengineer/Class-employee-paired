aec88c8912c6688cd73b8c6c3ed37160e0499334
Kaitlyn <kmiddlebrook2000@gmail.com>
2023-09-07 21:33:38 -0700

    Bonus Phase

--- manager.js
+++ manager.js
@@ -26,13 +26,13 @@ class Manager extends Employee {
     }
 }
 
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
-const raph = new Manager('Raphael', 90000, 'Ninja', leo);
-const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
-const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+// const splinter = new Manager('Splinter', 100000, 'Sensei');
+// const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+// const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+// const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+// const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
 
-console.log(splinter.calculateBonus(0.05)); // => 22500
+// console.log(splinter.calculateBonus(0.05)); // => 22500
 
 ///////////////////////////////////
 module.exports = Manager;
---
+++ test.js
@@ -0,0 +1,21 @@
+const Employee = require('./employee');
+const Manager = require('./manager');
+
+
+const hobbes = new Manager('Hobbes', 1000000, 'Founder')
+const calvin = new Manager('Calvin', 130000, 'Director', hobbes)
+const susie = new Manager('Susie', 100000, 'TA Manager', calvin)
+const lily = new Employee('Lily', 90000, 'TA', susie)
+const clifford = new Employee('Clifford', 90000, 'TA', susie)
+
+const hobbesBonus = hobbes.calculateBonus(0.05)
+const calvinBonus = calvin.calculateBonus(0.05)
+const susieBonus = susie.calculateBonus(0.05)
+const lilyBonus = lily.calculateBonus(0.05)
+const cliffordBonus = clifford.calculateBonus(0.05)
+
+console.log(hobbesBonus);
+console.log(calvinBonus);
+console.log(susieBonus);
+console.log(lilyBonus);
+console.log(cliffordBonus);
